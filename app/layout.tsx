0b41f0bfcda1123f9a1c365a83821c8077475375
Muhammad <usman.jameel615@gmail.com>
2026-02-13 15:19:07 +0500

    posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,8 @@ export const metadata: Metadata = {
   description: "The Hub For Every Dev Event you Mustn't Miss",
 };
 
+import { PostHogProvider } from "./providers";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,26 +31,28 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
-        <Navbar />
-        <div className="absolute min-h-screen z-[-1] top-0 inset-0">
-          <LightRays
-            raysOrigin="top-center-offset"
-            raysColor="#5dfeca"
-            raysSpeed={0.5}
-            lightSpread={0.9}
-            rayLength={1.4}
-            followMouse={true}
-            mouseInfluence={0.02}
-            noiseAmount={0}
-            distortion={0.01}
-            pulsating={false}
-            fadeDistance={1}
-            saturation={1}
-          />
-        </div>
-        <main>
-          {children}
-        </main>
+        <PostHogProvider>
+          <Navbar />
+          <div className="absolute min-h-screen z-[-1] top-0 inset-0">
+            <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.02}
+              noiseAmount={0}
+              distortion={0.01}
+              pulsating={false}
+              fadeDistance={1}
+              saturation={1}
+            />
+          </div>
+          <main>
+            {children}
+          </main>
+        </PostHogProvider>
       </body>
     </html>
   );
